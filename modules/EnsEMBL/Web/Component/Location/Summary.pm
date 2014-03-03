ff45fa60a6036c740e26655826052e3160ecae0a
Nick Langridge <nicklangridge@hotmail.com>
2014-03-03 11:38:15 +0000

    Add warning to barley whole chromosome displays ENSEMBL-2824

---
+++ modules/EnsEMBL/Web/Component/Location/Summary.pm
@@ -0,0 +1,38 @@
+=head1 LICENSE
+
+Copyright [1999-2014] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+## EG Plants hack - Display warning only for Barley 
+package EnsEMBL::Web::Component::Location::Summary;
+
+use strict;
+use previous qw(content);
+
+sub content {
+  my $self = shift;
+  
+  my $html = $self->PREV::content(@_);
+    
+  if ($html and $self->hub->species eq 'Hordeum_vulgare') {
+    $html .= $self->_warning('Non-standard assembly', '<p>The structure of this genome assembly differs significantly from conventional assemblies. Please see the detailed description of this assembly on the <a href="/Hordeum_vulgare/Info/Annotation/#assembly">barley about page</a></p>');
+  }
+  
+  return $html;
+}
+## 
+
+1;

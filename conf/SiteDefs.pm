f9e66d1f589b247aea32c84a814d3c9d870cd91c
Nick Langridge <nicklangridge@hotmail.com>
2015-08-14 14:47:14 +0100

    Add division flag

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -27,6 +27,7 @@ sub update_conf {
 
     $SiteDefs::ENSEMBL_BASE_URL          = 'http://plants.ensembl.org';
 
+    $SiteDefs::EG_DIVISION      = 'plants';
     $SiteDefs::ENSEMBL_SITETYPE = 'Ensembl Plants';
     $SiteDefs::SITE_NAME        = 'Ensembl Plants';
     $SiteDefs::SITE_FTP         = 'ftp://ftp.ensemblgenomes.org/pub/plants';

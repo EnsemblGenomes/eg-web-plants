b91842fb993421f73198edffe9cf4f1bdc2f92ac
Steve Trevanion <steve3@ebi.ac.uk>
2018-12-11 18:26:32 +0000

    remove out of date Google Search Console verification code; correct one will be picked up from ebi-plugins/common

--- modules/EnsEMBL/Web/Document/Element/Meta.pm
+++ modules/EnsEMBL/Web/Document/Element/Meta.pm
@@ -22,7 +22,6 @@ sub init {
   my $self = shift;
   $self->add('description', 'Ensembl Plants is a genome-centric portal for plant species of scientific interest');
   $self->add('keywords', 'Ensembl, Ensembl Genomes, genome, genome browser, comparative genomics, variation, SNPs, EST, mRNA, rna-Seq, orthologs, paralogs, synteny, assembly, genes, transcripts, translations, proteins, cereals, monocots, dicots, moss, Arabidsopsis, rice, maize, 1001 genomes, plants');
-  $self->add('google-site-verification', 'tC5CRODhAwiBEeIZY_EP_k7AaLGupandW7e1v3_s8ak');
 }
 
 1;

6b81f95cfc42b793834de59774854a6ff09e8531
Anne Lyle <ens-ap5@users.noreply.github.com>
2022-08-25 15:43:24 +0100

    Remove obsolete keywords

--- modules/EnsEMBL/Web/Document/Element/Meta.pm
+++ modules/EnsEMBL/Web/Document/Element/Meta.pm
@@ -21,7 +21,6 @@ package EnsEMBL::Web::Document::Element::Meta;
 sub init {
   my $self = shift;
   $self->add('description', 'Ensembl Plants is a genome-centric portal for plant species of scientific interest');
-  $self->add('keywords', 'Ensembl, Ensembl Genomes, genome, genome browser, comparative genomics, variation, SNPs, EST, mRNA, rna-Seq, orthologs, paralogs, synteny, assembly, genes, transcripts, translations, proteins, cereals, monocots, dicots, moss, Arabidsopsis, rice, maize, 1001 genomes, plants');
 }
 
 1;

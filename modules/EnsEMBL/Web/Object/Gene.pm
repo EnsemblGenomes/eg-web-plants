91d16c8c62657bc8ecc42bebaee90768ee3c8281
twalsh-ebi <twalsh@ebi.ac.uk>
2026-01-20 10:44:40 +0000

    Add messages on large gene families and unannotated paralogies (#102)
    * Add messages on large gene families and unannotated paralogies
    
    * Check for unannotated paralogies in ComparaParalogs
    
    * Update URL of large gene family blog post

--- modules/EnsEMBL/Web/Object/Gene.pm
+++ modules/EnsEMBL/Web/Object/Gene.pm
@@ -1,6 +1,6 @@
 =head1 LICENSE
 
-Copyright [2009-2024] EMBL-European Bioinformatics Institute
+Copyright [2009-2025] EMBL-European Bioinformatics Institute
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -27,6 +27,45 @@ sub insdc_accession {
 sub getReactomeXrefs() {
   my $self   = shift;
   return $self->Obj->get_all_DBEntries('Plant_Reactome_Pathway');
+}
+
+sub has_unannotated_paralogs {
+  my $self        = shift;
+  my $compara_db  = shift || 'compara';
+  my $consensus_clusterset_id = shift || 'default';
+
+  my $cache_key  = sprintf('_has_unannotated_paralogs_%s_%s', $compara_db, $consensus_clusterset_id);
+
+  if (!$self->{$cache_key}) {
+    my $args = {'stable_id' => $self->stable_id, 'cdb' => $compara_db};
+    my $member  = $self->get_compara_Member($args);
+
+    my $has_unannotated_paralogs = 0;
+    if ($member) {
+      my $gene_tree_adaptor = $member->adaptor->db->get_adaptor('GeneTree');
+      if ($gene_tree_adaptor) {
+        my $tree = $gene_tree_adaptor->fetch_default_for_Member($member, $consensus_clusterset_id);
+        if($tree && $tree->get_tagvalue('has_other_paralog_groups')) {
+          my $gtos_adaptor = $tree->adaptor->db->get_adaptor('GeneTreeObjectStore');
+          if ($gtos_adaptor) {
+            my $json_string = $gtos_adaptor->fetch_by_GeneTree_and_label($tree, 'other_paralog_groups');
+            if ($json_string) {
+              my $other_paralog_groups = JSON->new->decode($json_string);
+              while (my ($gdb_id, $rec) = each %{$other_paralog_groups}) {
+                if (grep {$_ == $member->gene_member_id} @{$rec->{'gene_member_ids'}}) {
+                  $has_unannotated_paralogs = 1;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    $self->{$cache_key} = $has_unannotated_paralogs;
+  }
+
+  return $self->{$cache_key};
 }
 
 1;
